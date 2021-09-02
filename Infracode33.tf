ed4cbac33b47e283a3a269d73feb9a96d9e8eb6f
Chuck Holland <83679580+holland5555@users.noreply.github.com>
2021-09-01 21:41:27 -0700

    Add files via upload

---
+++ Infracode33.tf
@@ -0,0 +1,10 @@
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-tf-test-bucket"
+  acl    = "public-read"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+Test Test Test
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# EuTest8
+# Test1
+
